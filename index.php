ba1cc3592ca01e45ad10a5c0442efa49678a0bad
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2019-01-04 12:52:31 +0100

    AJout de tables dans la base et création du bouton qui chosit les tables à ajouter

--- index.php
+++ index.php
@@ -20,10 +20,16 @@
         }
         //En fonction de la table choisi on veut créer des tables dedans
         if($db="Eleves"){
+            $ajout1=$dbh->exec("CREATE TABLE `Eleves`.`Act` ( `ActID` INT(1) NOT NULL , `Lieu` VARCHAR(100) NOT NULL , `Bus` INT(1) NOT NULL , `Theme` VARCHAR(100) NOT NULL , `Jour` INT(1) NOT NULL ) ENGINE = InnoDB; ");
+            $ajout2=$dbh->exec("CREATE TABLE `Eleves`.`Classes` ( `ClasID` INT(1) NOT NULL , `Enseignant` VARCHAR(100) NOT NULL)ENGINE = InnoDB ;");
+            $ajout3=$dbh->exec("CREATE TABLE `Eleves`.`Eleves` ( `ElevID` INT(2) NOT NULL , `Nom` VARCHAR(100) NOT NULL , `Age` INT(2) NOT NULL , `Ville` VARCHAR(100) NOT NULL  ) ENGINE = InnoDB;" );
+            //table Repartition je ne la comprends pas ...
             $table1="Activites";
             $table2="Classes";
             $table3="Eleves";
+            $table4="Repartition";
         }if($db="Clients"){
+            
             $table1="Client";
             $table2="Commande";
             $table3="Detail";
@@ -37,7 +43,6 @@
     }?>
 
     else{
-        
         <head>
         Mon DM de BDD
         </head>
@@ -47,13 +52,23 @@
         </p>
         <select name="choix">
         <option value="Livres">Livres</option>
-        <option value="Eleves">Ecole</option>
-        <option value="Clients">Entreprise</option>
+        <option value="Eleves">Eleves</option>
+        <option value="Clients">Clients</option>
         <br/> <!-- Pourquoi ne fait pas un retour à la ligne ? -->
         <input type="submit" name="valider" value="OK"/>
         </select>
         <p> Ajout de ligne dans les tables à partir des fichiers csv
         </p>
+        <?php
+       echo '<select name="choix2">
+        <option value="'.$table1.'">'.$table1.'</option>
+        <option value="'.$table2.'">'.$table2.'</option>
+        <option value="'.$table3.'">'.$table3.'</option>
+        <option value="'.$table4.'">'.$table4.'</option>
+        <input type="submit" name="valider2" value="OK"/>
+        </select>';
+        ?>
+       
         <p> Bouton permettant de se diriger sur divers pages cf questions suivantes
         </p>
         <p> Bouton qui efface toutes la table de données ainsi que toute trace d exectution sur la machine

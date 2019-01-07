bb785df9c52296f4001e8d1df9a4b34079d280fe
iris.dum <iris.dumeur6@etu.univ-lorraine.fr>
2019-01-07 22:49:34 +0100

    Ecriture de la bonne commande SQL en fonction des elements à rajouter : possible d'optimiser cependant ne trouve pas la colonne

--- ajout_table.php
+++ ajout_table.php
@@ -5,6 +5,7 @@ Modification des tables
 </head>
 <p>
 <?php
+
     if(!(isset($_POST['valider2'])) && !(isset($_POST['valider3']))){ //L'option valider 2 correspond au choix de la table valider 3 au choix du fichier csv
         $db = $_POST['choix']; /**la récupération données non protégé*/
         echo " La base $db a été crée";
@@ -68,6 +69,7 @@ Modification des tables
                     }
                 }if($db=="Livres"){
                         $ajout1=$dbh->exec("CREATE TABLE IF NOT EXISTS `Livres`.`Auteur` (`id_auteur` INT(1) NOT NULL,`nom_auteur` varchar(20) DEFAULT NULL,`pre_nom_auteur` varchar(20) DEFAULT NULL,`Naissance` DATE DEFAULT NULL,`Mort` DATE DEFAULT NULL,`Nationalite` VARCHAR(20)) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+                        //$ajout12=$dbh->exec("ALTER TABLE `Auteur` ADD PRIMARY KEY(`id_auteur`);");
                         $ajout2=$dbh->exec("CREATE TABLE IF NOT EXISTS `Livres`.`Ecrit_par` (`id_auteur` INT(1) NOT NULL,`id_livre` INT(1)) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
                         $ajout3=$dbh->exec("CREATE TABLE IF NOT EXISTS `Livres`.`Edite_par` (`id_editeur` INT(1) NOT NULL,`id_livre` INT(1)) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
                         $ajout4=$dbh->exec("CREATE TABLE IF NOT EXISTS `Livres`.`Editeur` (`id_editeur` INT(1) NOT NULL,`nom_editeur` VARCHAR(30),`site_web` VARCHAR(40)) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
@@ -78,48 +80,85 @@ Modification des tables
                         $table5="Livre";
                         $table4="Editeur";
                         //echo "je suis dans la table Livres";
-                    }
+                }
                     ?>
-        <form method="post" action="ajout_table.php" enctype="multipart/form-data">
+            <form method="post" action="ajout_table.php" enctype="multipart/form-data">
+            <?php
+                echo "Choisir la table à ajouter à la base $db";
+                echo '<select name="choix2">
+                <option value="'.$table1.'">'.$table1.'</option>
+                <option value="'.$table2.'">'.$table2.'</option>
+                <option value="'.$table3.'">'.$table3.'</option>
+                <option value="'.$table4.'">'.$table4.'</option>
+                <option value="'.$table5.'">'.$table5.'</option>
+                <input type="submit" name="valider2" value="OK"/>
+                </select>';
+                ?>
+                <input type="hidden" name="db_name" value= <?php echo "$db";?> />
+            </form>
         <?php
-            echo "Choisir la table à ajouter à la base $db";
-            echo '<select name="choix2">
-            <option value="'.$table1.'">'.$table1.'</option>
-            <option value="'.$table2.'">'.$table2.'</option>
-            <option value="'.$table3.'">'.$table3.'</option>
-            <option value="'.$table4.'">'.$table4.'</option>
-            <option value="'.$table5.'">'.$table5.'</option>
-            <input type="submit" name="valider2" value="OK"/>
-            </select>';
-        ?>
-        </form>
-        <?php
-        }if(isset($_POST['valider2'])){ //On crée le bouton pour trouver la table CSV à ajouter
+            }
+            if(isset($_POST['valider2'])){ //On crée le bouton pour trouver la table CSV à ajouter
             echo "Sélectionne le fichier $_POST[choix2]";
             $chosen_table=$_POST['choix2'];
+            echo "$chosen_table";
+            $db=$_POST['db_name'];
+            echo "de la base $db";
             ?>
         <form method = "post" action="ajout_table.php" enctype="multipart/form-data">
         <label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
         <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
         <input type="file" name="mon_fichier" id="mon_fichier" /> <br/>
         <input type="submit" name="valider3" value="OK"/>
+        <input type="hidden" name="db_name" value= <?php echo "$db " ;?> />
+        <input type="hidden" name="tab" value=<?php echo "$chosen_table " ;?> />
         </form>
     <?php
     }
     if(isset($_POST['valider3'])){ // le fichier a été trouvé dans le finder        $path=$_FILES['mon_fichier']['tmp_name'];
         $path=$_FILES['mon_fichier']['tmp_name'];
-        echo"je suis dans le valider 3 avec $path";
+        //echo"je suis dans le valider 3 avec $path";
+        $host="localhost";
+        $root="root";
+        $root_password="rootpass";
+        $user='newuser';
+        $pass='newpass';
+        $db=rtrim($_POST['db_name'],"/"); // j'ai un caractère / qui s'est ajouté mystérieusement dans le nom de ma base et table avec le dernier POST ...
+        $chosen_table=rtrim($_POST['tab'],"/");
+        echo "je suis la table $chosen_table";
+        $host="localhost";
+        $root="root";
+        $root_password="rootpass";
+        $user='newuser';
+        $pass='newpass';
+        try
+        {
+            $dbh = new PDO("mysql:host=localhost",'root','root');
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbh->exec("CREATE DATABASE IF NOT EXISTS `$db`; CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass'; GRANT ALL ON `$db`.* TO '$user'@'localhost';FLUSH PRIVILEGES;")
+            or die(print_r($dbh->errorInfo(), true));
+            $dbh->exec("use $db;");
+            
+            echo "La base de données est $db est utilisé !!!";
+        }
+        catch (PDOException $e) {
+            die("DB ERROR: ". $e->getMessage());
+        }
         if($_FILES["mon_fichier"]["size"] > 0)
         {   $file = fopen($path, "r");
-            $Name_col=fgetcsv($file, 10000, ",");
-            $num=count(Name_col);
-            while (($getData = fgetcsv($file, 10000, ",")) !== FALSE)
+            $Name_col=fgetcsv($file, 10000, ";");
+            $num=count($Name_col);
+            //echo "$Name_col[0]";
+            while (($getData = fgetcsv($file, 10000, ";")) !== FALSE)
             {
-                print_r($getData);
+                //print_r($getData);
                 for($j=0;$j<$num;$j++){
-                    $sql = "INSERT into $chosen_table '".$Name_col[$j]."' VALUES ('".$getData[$j]."')";
+                    //echo "$chosen_table";
+                    $col=ltrim(rtrim($Name_col[$j],")"),"(");
+                    $sql = "INSERT INTO `$chosen_table` (`$col`) VALUES ( $getData[$j] )";
+                    echo $sql;
                     $ajout_table=$dbh->exec($sql);
-                //	$result = $sql->query("SELECT * FROM Client");
+                    //$result = $dbh->query($sql);
                 //	if(!isset($result))
                 //	{
                 //		echo "ici";

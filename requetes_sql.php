353e10ded03df1fa51b800a78abd2903089de046
Clemence <clemence.vast@mines-nancy.org>
2019-01-11 00:21:22 +0100

    menu deroulant

--- requetes_sql.php
+++ requetes_sql.php
@@ -10,7 +10,15 @@ $db = $_POST['db_name'];
 echo"Nom de la table actuellement utilisée :  $db"; ?>
 </p>
 <p>
-<input type="button" value= "requete1" />
-</p>
-<input type="button" value= "requete2" />
-
+            <form method="post" action="requetes_sql.php" enctype="multipart/form-data">
+            <?php
+                echo "Quelle requête souhaitez-vous effectuer sur $db ?";
+                echo '<select name="requete">
+                <option value="'.$r1.'">'.$r1.'</option>
+                <option value="'.$r2.'">'.$r2.'</option>
+                <option value="'.$r3.'">'.$r3.'</option>
+                <option value="'.$r4.'">'.$r4.'</option>
+                <option value="'.$r5.'">'.$r5.'</option>
+                <input type="submit" name="valider" value="OK"/>
+                </select>';
+?>
